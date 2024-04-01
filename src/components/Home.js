88552f5e6f845b3e7e845eb11fbef0c0516e2cf7
kristinamacheva <macheva.kristina@gmail.com>
2024-04-01 17:22:23 +0300

    Create handlers for adding and deleting items from the SelectedFood component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,48 @@
-import { Button } from "react-bootstrap";
+import { Button,  Container } from "react-bootstrap";
 import FoodSearch from "./FoodSearch";
 import SelectedFood from "./SelectedFood";
 import { useState } from "react";
 
 export default function Home() {
-    const [selectedFoodItems, setSelectedFoodItems] = useState([
-        {
-          "id": "548812",
-          "description": "Pizza",
-          "kcal": 410,
-          "protein": 17,
-          "fat": 10,
-          "carbs": 40
-        },
-        {
-            "id": "214155",
-            "description": "Burger",
-            "kcal": 400,
-            "protein": 15,
-            "fat": 19,
-            "carbs": 50
-        },
-        {
-          "id": "684752",
-          "description": "Pasta",
-          "kcal": 350,
-          "protein": 6,
-          "fat": 5,
-          "carbs": 33
-        },
-      ]);
+    const [selectedFoodItems, setSelectedFoodItems] = useState([]);
+    
+    const foodItemAddHandler = (food) => {
+        console.log('Food clicked:', food);
+
+        const isAlreadyAdded = selectedFoodItems.some(item => item.id === food.id);
+
+        if (!isAlreadyAdded) {
+            setSelectedFoodItems(state => ([
+                ...state,
+                food,
+            ]));
+        } else {
+            console.log('This food is already added.');
+        }
+    }
+
+    const foodItemRemoveHandler = (foodToRemove) => {
+        console.log('Food removed:', foodToRemove);
+        
+        setSelectedFoodItems(state => (
+            state.filter(food => food !== foodToRemove)
+        ));
+    }
 
     return (
-        <div className="home-container">
-            <SelectedFood selectedFoodItems={selectedFoodItems}/>
-            <FoodSearch />
+        <Container>
+            <SelectedFood 
+                selectedFoodItems={selectedFoodItems} 
+                onFoodItemRemove={foodItemRemoveHandler} 
+            />
+            
+            <FoodSearch 
+                onFoodItemAdd={foodItemAddHandler} 
+            />
+
             <div className="d-flex justify-content-end mt-3">
                 <Button variant="primary" type="submit">Add Food</Button>
             </div>
-        </div>
+        </Container>
     );
 }
