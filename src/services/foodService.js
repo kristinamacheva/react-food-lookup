81b74369bb511e7495be2597ba3518a05eb8dc06
kristinamacheva <macheva.kristina@gmail.com>
2024-04-01 13:16:08 +0300

    Create foodService.js - getAll and create

---
+++ src/services/foodService.js
@@ -0,0 +1,34 @@
+const baseUrl = 'http://localhost:3005/foods';
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    const data = Object.values(result);
+
+    return data;
+};
+
+export const create = async (data) => {
+    const body = {
+        id: data.id,
+        description: data.description,
+        kcal: data.kcal,
+        protein: data.protein,
+        fat: data.fat,
+        carbs: data.carbs,
+    };
+
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    return result;
+};
